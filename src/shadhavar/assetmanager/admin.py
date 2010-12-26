7d8dcaa4f17435c3a6532cb038ce15c478039eab
Bas Pape <baspape@gmail.com>
2010-12-26 10:52:52 +0100

    Enable admin interface

---
+++ src/shadhavar/assetmanager/admin.py
@@ -0,0 +1,17 @@
+from assetmanager.models import * # import all models
+from django.contrib import admin
+
+admin.site.register(Datacentre)
+admin.site.register(Serverroom)
+admin.site.register(Rack)
+admin.site.register(Device)
+admin.site.register(Router)
+admin.site.register(Server)
+admin.site.register(Switch)
+admin.site.register(KVM)
+admin.site.register(UPS)
+admin.site.register(Other)
+admin.site.register(PDU)
+admin.site.register(Subnet)
+admin.site.register(Networkinterface)
+admin.site.register(VM)
