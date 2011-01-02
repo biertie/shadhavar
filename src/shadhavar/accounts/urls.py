13ecc4c0b56b932ac5b97ee1083c60ec70582ed0
Bas Pape <baspape@gmail.com>
2011-01-02 13:45:01 +0100

    Use default template names

--- src/shadhavar/accounts/urls.py
+++ src/shadhavar/accounts/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('accounts',
-    (r'^login/', login, {'template_name': 'accounts/login.html'}),
-    (r'^logout/', logout, {'template_name': 'accounts/logout.html', 'next_page': '/'}),
+    (r'^login/', login, {}),
+    (r'^logout/', logout, {}),
 )
