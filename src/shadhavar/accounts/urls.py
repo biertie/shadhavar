4c4125caa675617016e7c3d62ed15de9e3c96d0e
Bas Pape <baspape@gmail.com>
2011-01-02 18:11:15 +0100

    Added password reset stuff

--- src/shadhavar/accounts/urls.py
+++ src/shadhavar/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_reset,\
+    password_reset_done
 
 urlpatterns = patterns('accounts',
-    (r'^login/', login, {}),
-    (r'^logout/', logout, {}),
+    (r'^login/', login, {}, 'accounts_login'),
+    (r'^logout/', logout, {}, 'accounts_logout'),
+    (r'^reset/done/', password_reset_done, {}, 'accounts_reset_done'),
+    (r'^reset/', password_reset, {'post_reset_redirect': '/accounts/reset/done'}, 'accounts_reset'),
 )
