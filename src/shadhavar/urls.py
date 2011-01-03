8f1be5f02b620e0fd58e7b9d96cc5721b831c6fa
Bas Pape <baspape@gmail.com>
2011-01-03 21:14:38 +0100

    Added the R from crud for both datacentre and serverroom

--- src/shadhavar/urls.py
+++ src/shadhavar/urls.py
@@ -23,11 +23,13 @@ urlpatterns += patterns('django.views.generic',
     (r'^assets/datacentre/(?P<object_id>[0-9]+)/$', 'list_detail.object_detail', {'queryset': assetmanager.models.Datacentre.objects.all()}, 'datacentre_detail'),
     (r'^assets/datacentre/add/$', 'create_update.create_object', {'model': assetmanager.models.Datacentre, 'post_save_redirect': '/assets/datacentre/'}, 'datacentre_add'),
     (r'^assets/datacentre/edit/(?P<object_id>[0-9]+)/$', 'create_update.update_object', {'model': assetmanager.models.Datacentre, 'post_save_redirect': '/assets/datacentre/'}, 'datacentre_update'),
+    (r'^assets/datacentre/delete/(?P<object_id>[0-9]+)/$', 'create_update.delete_object', {'model': assetmanager.models.Datacentre, 'post_delete_redirect': '/assets/datacentre/'}, 'datacentre_delete'),
 
     (r'^assets/serverroom/$', 'list_detail.object_list', {'queryset': assetmanager.models.Serverroom.objects.all()}, 'serverroom_index'),
     (r'^assets/serverroom/(?P<object_id>[0-9]+)/$', 'list_detail.object_detail', {'queryset': assetmanager.models.Serverroom.objects.all()}, 'serverroom_detail'),
     (r'^assets/serverroom/add/$', 'create_update.create_object', {'model': assetmanager.models.Serverroom, 'post_save_redirect': '/assets/serverroom/'}, 'serverroom_add'),
     (r'^assets/serverroom/edit/(?P<object_id>[0-9]+)/$', 'create_update.update_object', {'model': assetmanager.models.Serverroom, 'post_save_redirect': '/assets/serverroom/'}, 'serverroom_update'),
+    (r'^assets/serverroom/delete/(?P<object_id>[0-9]+)/$', 'create_update.delete_object', {'model': assetmanager.models.Serverroom, 'post_delete_redirect': '/assets/serverroom/'}, 'serverroom_delete'),
 )
 
 urlpatterns += patterns('',
