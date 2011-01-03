f9fcd5cd42244462e94ff2a99cabed3b95cc64b0
Bas Pape <bas@desktop-bas.localdomain>
2011-01-03 08:04:24 +0100

    Add change password stuff

--- src/shadhavar/accounts/urls.py
+++ src/shadhavar/accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('accounts',
     (r'^reset/done/$', password_reset_done, {}, 'accounts_reset_done'),
     (r'^reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {}, 'accounts_reset_confirm'),
     (r'^reset/$', password_reset, {'post_reset_redirect': '/accounts/reset/done'}, 'accounts_reset'),
+    (r'^password/$', password_change, {}, 'accounts_password_change'),
+    (r'^password/done$', password_change_done, {}, 'accounts_password_change_done'),
 )
