447407b7ef40e7173bb14c31f5054a7b77d76c46
Bas Pape <baspape@gmail.com>
2011-01-03 13:02:07 +0100

    Don't require login for password resets

--- src/shadhavar/middleware.py
+++ src/shadhavar/middleware.py
@@ -16,7 +16,7 @@ class RequireLoginMiddleware(object):
         self.require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/accounts/login/')
 
     def process_request(self, request):
-        if request.path != self.require_login_path and request.user.is_anonymous():
+        if request.path != self.require_login_path and not request.path.startswith('/accounts/reset/') and request.user.is_anonymous():
             if request.POST:
                 return login(request)
             else:
