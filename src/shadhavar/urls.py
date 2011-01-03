f2fd67ca3ca6371bd9181286192de4838afcc72b
Bas Pape <baspape@gmail.com>
2011-01-03 19:54:10 +0100

    Added CRUD using generic views for datacentre

--- src/shadhavar/urls.py
+++ src/shadhavar/urls.py
@@ -16,6 +16,15 @@ urlpatterns = patterns('',
     (r'^accounts/password/done$', django.contrib.auth.views.password_change_done, {}, 'accounts_password_change_done'),
 )
 
+import django.views.generic
+import assetmanager.models
+urlpatterns += patterns('django.views.generic',
+    (r'^assets/datacentre/$', 'list_detail.object_list', {'queryset' : assetmanager.models.Datacentre.objects.all()}, 'datacentre_index'),
+    (r'^assets/datacentre/(?P<object_id>[0-9]+)/$', 'list_detail.object_detail', {'queryset': assetmanager.models.Datacentre.objects.all()}, 'datacentre_detail'),
+    (r'^assets/datacentre/add/$', 'create_update.create_object', {'model': assetmanager.models.Datacentre, 'post_save_redirect': '/assets/datacentre/'}, 'datacentre_add'),
+    (r'^assets/datacentre/edit/(?P<object_id>[0-9]+)/$', 'create_update.update_object', {'model': assetmanager.models.Datacentre, 'post_save_redirect': '/assets/datacentre/'}, 'datacentre_update'),
+)
+
 urlpatterns += patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
