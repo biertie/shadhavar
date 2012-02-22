d1b2febb7b9c3a10be4e8ec0a0f7389464f53b1c
Bas Pape <baspape@gmail.com>
2012-02-22 13:38:26 +0100

    Remove useless import

--- src/shadhavar/views/base.py
+++ src/shadhavar/views/base.py
@@ -1,6 +1,5 @@
 from shadhavar import settings
 from django.views.generic import TemplateView
-import assetmanager.models as assetmodels
 
 class BaseView(TemplateView):
     def get_context_data(self, **kwargs):
